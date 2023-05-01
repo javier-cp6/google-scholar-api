01a08ef3aebb6fec808106fc50fbd5b4f7c595ee
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2023-04-30 22:59:29 -0500

    initial commit

---
+++ src/main/java/com/company/googlescholarapi/GooglescholarapiApplication.java
@@ -0,0 +1,13 @@
+package com.company.googlescholarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglescholarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglescholarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/googlescholarapi/GooglescholarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.googlescholarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglescholarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
